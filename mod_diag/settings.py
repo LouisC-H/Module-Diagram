3c7088bc271621f5b39bb5c0ccaaa0212654e481
LouisC-H <louisclementharris@yahoo.co.uk>
2021-02-26 10:11:30 +0000

    Finished the merge with Eilish's code (hopefully?)

--- mod_diag/settings.py
+++ mod_diag/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "django_static_fontawesome",
+    'department'
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
 os.path.join(BASE_DIR, 'static')
 ]
+
+# For Eilish's branch
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR
--- mod_diag/urls.py
+++ mod_diag/urls.py
@@ -16,7 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from django.conf import settings
+from django.conf.urls.static import static
+from department import views
+
 urlpatterns = [
     path('diagram/', include('diagram.urls')),
     path('admin/', admin.site.urls),
 ]
+#     path('homepage/', views.homepage, name='homepage'),
+#     path('module/<int:module_id>', views.detail, name='detail'),
+# ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
