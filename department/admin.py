d42ad4e1d80d74bba03fa6674c73398296465055
Eilish <e.ogrady@exeter.ac.uk>
2021-03-05 08:36:56 +0000

    Update admin.py

--- department/admin.py
+++ department/admin.py
@@ -5,4 +5,4 @@ from .models import Module
 
 @admin.register(Module)
 class departmentadmin(admin.ModelAdmin):
-    list_display = ['code', 'department','year','term','credits','lecturer','core_Natural_Sciences','core_Mathematics','core_Physics_BPhys','core_Biochemistry','core_Physics_BPhys','core_Biological_Sciences','core_Biological_and_Medicinal_Chemistry', 'natural_Sciences_History', 'topic_pathway', 'Co_req','Pre_req' ] 
+    list_display = [ ]
