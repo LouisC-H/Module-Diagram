0b52b7e3a10239d7987d04e811abb057b0ee6c8d
LouisC-H <louisclementharris@yahoo.co.uk>
2021-05-06 11:32:38 +0100

    Improved comments

--- diagram/models.py
+++ diagram/models.py
@@ -6,7 +6,7 @@ import numpy as np
 class Map():
     '''
     This class sits in the background and organises all of the (relevant)
-    modules from the database into a diagram.
+    modules from the database into a module map.
     '''
 
     def __init__(self):
@@ -19,41 +19,38 @@ class Map():
         Initialise variables and call functions
         '''
 
-        #Find a way to let users set this themselves
-        self.chosen_departments = ['NatSci', 'Phy', 'Bio','Maths']
-        # self.chosen_departments = ['NatSci']
-        self.years = ['1', '2', '3', '4']
-        self.chosen_years = [True, True, True, True]
-
-        #Find dimensions of data matrix
-        self.columns_dict = {}
-        self.num_columns = 0
-
-        #Store disordered module data
-        self.DiagBox_list = []
-        self.table_data = []
-
-        #Beautify each department's columns with a background colour
+        #Store some default data
         self.potential_departments = ['CSM', 'Comp', 'Eng', 'Maths', 'NatSci', 'Phy', 'Bio', 'Geo', 'Other']
         self.departments_in_full = ['CSM', 'Computing', 'Engineering', 'Mathematics', 'Natural Science', 'Physics', 'Biosciences', 'Geography', 'Other']
         self.dept_colours = ['#b39559', '#ffb380', '#e67373', '#73e6e6',
                             '#cf8cde', '#99bbff', '#95e67c', '#e6e673', '#ffffff']
-        self.chosen_colours = []
+        self.years = ['1', '2', '3', '4']
 
+        #Find a way to let users set this themselves
+        self.chosen_departments = ['NatSci', 'Phy', 'Bio','Maths']
+        self.chosen_years = [True, True, True, True]
 
-        self.debug_iteration = False
+        #Prepare to store the dimensions of data matrix
+        self.columns_dict = {}
+        self.num_columns = 0
 
+        #This will store the list of DiagBox objects, each of which corresponds to
+        # one module in the diagram
+        self.DiagBox_list = []
 
+        #This will store the data to be exported to the Django Template file
+        self.table_data = []
+
+        #Call the functions to do the data analysis
         self.set_relevant_years()
         self.initialise_DiagBoxes()
         self.set_diagram_columns()
         self.create_data_table()
 
-
-
     def set_relevant_years(self):
         '''
-        For any years set to "False", change that value in self.years to "False"
+        For any years that the user doesn't want to see (set to "False"), change
+        that value in self.years to "False"
         '''
 
         for i in range(len(self.chosen_years)):
@@ -63,7 +60,8 @@ class Map():
     def initialise_DiagBoxes(self):
         '''
         From each module in the databse, create one DiagBox object (as long as
-        the module belongs to a dept/year in scope of what the user wants)
+        the module belongs to a department and year that's in scope of what the
+        user wants to see)
         '''
 
         for department in self.chosen_departments:
@@ -81,43 +79,24 @@ class Map():
 
             #Make a dictionary, with each item corresponding to a department
             if module.database.department not in self.columns_dict:
-                # self.depts_list.append(module.database.department)
                 self.columns_dict[module.database.department] = {}
 
-            #Assign the dictionary index number to the module
+            #Assign the dictionary index number to the module's DiagBox object
             module.dept_number = list(self.columns_dict.keys()).index(module.database.department)
 
             #Within the department dictionary item, create a dictionary of
-            # module categories (corresponding to columns in the final diagram)
+            # module categories (corresponding to the columns in the final diagram)
             if module.database.category not in self.columns_dict[module.database.department]:
                 self.columns_dict[module.database.department][module.database.category] = self.num_columns
                 self.num_columns +=1
 
+            #Store a reference to the column in which the DiagBox will be placed
             module.category_num_unordered = self.columns_dict[module.database.department][module.database.category]
-
-        # self.order_diagram_columns()
-
-    def order_diagram_columns(self):
-        '''
-        From the list of modules (Diagox objects), assign to each one a number
-        according to their department and category.
-        '''
-        new_index = 0
-        #For each column in the diagram...
-        for department in self.columns_dict:
-            for category in self.columns_dict[department]:
-                # find which unordered index corresponded to it...
-                old_index = self.columns_dict[department][category]
-                for module in self.DiagBox_list:
-                    if module.category_num_unordered == old_index:
-                        # and set any modules in that category to the new, ordered index number
-                        module.column_index = new_index
-                self.columns_dict[department][category] = new_index
-                new_index += 1
 
     def create_data_table(self):
         '''
-        Create the table of nested data passed to the Django template to construct the diagram
+        Create the table of nested data, which will be passed to the Django
+        template to construct the diagram
         '''
         dept_index = 0
         #Within each department ...
@@ -133,7 +112,7 @@ class Map():
                 self.table_data[dept_index][0].append([])
                 # are the years that a module could be taken in. Within each year ...
                 for year in range(4):
-                    # are some modules, grouped together.
+                    # are some modules.
                     self.table_data[dept_index][0][category_index].append([
                             self.group_modules(dept_index, category_number, year), self.years[year]])
 
@@ -142,14 +121,19 @@ class Map():
 
     def group_modules(self, dept_index, category_number, year):
         '''
-        Find the modules in the required department/category/year, then prepare
+        Find the modules in the required category and year, then prepare
         to group them together so that they take less space on the diagram
         '''
-        modules_ungrouped = [module for module in self.DiagBox_list if module.dept_number == dept_index and module.category_num_unordered == category_number and module.year_index == year]
+        #Find the list of relevant modules
+        modules_ungrouped = [module for module in self.DiagBox_list if module.category_num_unordered == category_number and module.year_index == year]
 
+        #Create an empty list, which will be filled with the modules from
+        # modules_ungrouped once they have been grouped
         grouped_sublist = []
 
-        #Keep grouping modules until all of them are grouped.
+        #At each iteration, one or more modules are grouped together and moved
+        # from modules_ungrouped to grouped_sublist. Continue until modules_ungrouped
+        # is empty.
         while len(modules_ungrouped) > 0:
             modules_ungrouped, grouped_sublist = self.group_first_module(modules_ungrouped, grouped_sublist)
 
@@ -161,17 +145,22 @@ class Map():
         This could result in 1, 2 or 3 modules grouped together.
         '''
 
-        sum_module_height = [0, 0, 0]
+        #If a module runs across a term, it's "height" on that term will be 1,
+        # otherwise it will be 0. EG: a term 2 module will have a "height" of
+        # [0, 1, 0]. Here, we prepare to store the sum of the grouped modules'
+        # heights, which should never exceed 1 at any point.
         compatible_modules = []
+        sum_module_height = [0, 0, 0]
 
-        #The grouping will definitely contain the first module
+        #The grouping will definitely contain the first module from the
+        # modules_ungrouped list.
         first_module_height = modules_ungrouped[0].height
         sum_module_height = np.add(sum_module_height, first_module_height)
         compatible_modules.append(modules_ungrouped[0])
         modules_ungrouped.remove(modules_ungrouped[0])
 
-        #For every other module on the list, try adding it to the grouping,
-        # reject this if any two modules overlap across the same term(s).
+        #For every other module in the modules_ungrouped list, try adding it to
+        # the grouping, then check if any two grouped modules' terms overlap.
         for module in modules_ungrouped:
             sum_module_height = np.add(sum_module_height, module.height)
 
@@ -189,14 +178,13 @@ class Map():
     def order_modules(self, compatible_modules):
         '''
         When passing in a list of up to 3 modules, whose terms don't overlap,
-        return the same list, with the modules ordered by term (adding spacer
-        elements to bump modules down correctly if required)
+        return the list with the modules ordered by term (adding empty "spacer"
+        elements if required)
         '''
 
         ordered_modules = []
         term_1 = False
         term_2 = False
-        term_3 = False
 
         #Check for modules that run in term 1
         for module in compatible_modules:
@@ -204,9 +192,11 @@ class Map():
                 ordered_modules.append(module.module_information())
                 compatible_modules.remove(module)
                 term_1 = True
+                # Check if they also run across in term 2
                 if module.height[1] == 1:
                     term_2 = True
-        #If not, add a spacer
+
+        #If there are none, add a spacer
         if term_1 == False:
             ordered_modules.append(["spacer", -1])
 
@@ -221,6 +211,7 @@ class Map():
         if term_2 == False:
             ordered_modules.append(["spacer", -1])
 
+        #Check for modules that run in term 3 only
         for module in compatible_modules:
             if module.height[2] == 1:
                 ordered_modules.append(module.module_information())
@@ -231,21 +222,25 @@ class Map():
 
 class DiagBoxes:
     '''
-    This class is used to store and manipulate data in the generation of the diagram
+    This class is used to store and manipulate data pertaining to each module in
+    the database.
     '''
 
     def __init__(self, module):
 
         self.database = module
 
+        #Store data relating to its horizontal position
         self.dept_number = None
         self.category_num_unordered = None
 
+        #Store data relating to its vertical position
         self.year_index = None
         self.term_identifier = None
         self.height = [1, 1, 1]
 
         self.set_diagram_row()
+
 
     def __str__(self):
         return self.database.name
@@ -271,7 +266,8 @@ class DiagBoxes:
 
     def set_diagram_height(self):
         '''
-        Work out the vertical width that the module will take up, by the terms that it takes up
+        Work out the vertical width that the module will take up within a section,
+        according to the terms across which it runs
         '''
 
         if self.term_identifier == 1:
@@ -282,12 +278,13 @@ class DiagBoxes:
             self.height = [0, 0, 1]
         elif self.term_identifier == 12:
             self.height = [1, 1, 0]
-        elif self.term_identifier == 123:
-            self.height = [1, 1, 1]
         else:
             self.height = [1, 1, 1]
 
     def module_information(self):
+        '''
+        Return the data that is to be exported to the Django template file
+        '''
         return [self.database.name, self.term_identifier, self.database.website, self.database.credits, self.database.pre_req, self.database.co_req, self.database.code]
 
 class Module(models.Model):
